8c76b929a60763c8699e43696b170e16fd06745b
verphen <verphen@163.com>
2015-08-11 23:57:20 +0800

    update

--- src/main/java/cn/effine/controller/UserController.java
+++ src/main/java/cn/effine/controller/UserController.java
@@ -2,14 +2,49 @@ package cn.effine.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-	@RequestMapping("login")
-	public String login(String username, String password) {
+	/**
+	 * 用户注册
+	 * 
+	 * @param username
+	 *            用户名
+	 * @param password
+	 *            密码
+	 * @return
+	 */
+	@RequestMapping("signup")
+	@ResponseBody
+	public String signUp(String username, String password) {
 		return "login";
 	}
 
+	/**
+	 * 用户登录
+	 * 
+	 * @param username
+	 *            用户名
+	 * @param password
+	 *            密码
+	 * @return
+	 */
+	@RequestMapping("signin")
+	@ResponseBody
+	public String signIn(String username, String password) {
+		return "login";
+	}
+
+	/**
+	 * 用户注销
+	 * 
+	 * @return
+	 */
+	@RequestMapping("signout")
+	public String signOut() {
+		return "login";
+	}
 }
