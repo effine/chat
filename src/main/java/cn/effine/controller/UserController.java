0e7ae6427407899ee0e7177f493dc08734ced895
effine-win <verphen@163.com>
2016-01-12 00:25:03 +0800

    update

--- src/main/java/cn/effine/controller/UserController.java
+++ src/main/java/cn/effine/controller/UserController.java
@@ -7,13 +7,19 @@
 
 package cn.effine.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.effine.model.User;
+import cn.effine.service.UserService;
+
 @Controller
 @RequestMapping("user")
 public class UserController {
+	@Autowired
+	private UserService userService;
 
 	/**
 	 * 用户注册
@@ -42,6 +48,8 @@ public class UserController {
 	@RequestMapping("signin")
 	@ResponseBody
 	public String signIn(String username, String password) {
+		User user = userService.getUser(1);
+		System.out.println("name：" + user.getName());
 		return "login";
 	}
 
